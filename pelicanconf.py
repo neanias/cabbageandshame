80a67d688ee182b8840b00d87d242dc27337749d
William Mathewson <wncmathewson@me.com>
2019-12-27 21:46:51 +0000

    Add auto-generated content

---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Will Mathewson, Nathan Ward'
+SITENAME = 'cabbageandshame'
+SITEURL = f'{SITENAME}.co.uk'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
+
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.smarty': {},
+    },
+}
+
+# Blogroll
+LINKS = (('Source', 'https://github.com/neanias/cabbageandshame'),)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
