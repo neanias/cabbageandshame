2aa926b0c8cf7ffe67637326fcd494fd72e70f15
William Mathewson <wncmathewson@me.com>
2020-01-02 02:14:24 +0000

    Add modified Hyde theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,14 +3,18 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Will Mathewson, Nathan Ward'
-SITENAME = 'cabbageandshame'
-SITEURL = f'www.{SITENAME}.co.uk'
+PROFILE_IMAGE = 'IMG_0825.png'
+BIO = "A post-modern identity crisis with Will & Nathan"
+SITENAME = 'Cabbage and Shame'
+SITEURL = ''
+THEME = 'theme'
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = 'en'
+GITHUB_URL = 'https://github.com/neanias/cabbageandshame'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -30,10 +34,13 @@ MARKDOWN = {
 }
 
 # Blogroll
-LINKS = (('Source', 'https://github.com/neanias/cabbageandshame'),)
+SIDEBAR_LINKS = (('Categories', '/categories'),
+                 ('Tags', '/tags'),
+                 ('Authors', '/authors'),
+                 )
 
 # Social widget
-SOCIAL = ()
+SOCIAL = (('github', 'https://github.com/neanias/cabbageandshame'),)
 
 DEFAULT_PAGINATION = 10
 
