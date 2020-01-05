ebe6449695c08a1d7794fac87d1790e5c16a89a0
William Mathewson <wncmathewson@me.com>
2020-01-05 23:08:05 +0000

    Run black over code base

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,22 +4,22 @@ from __future__ import unicode_literals
 
 from pymdownx import emoji
 
-AUTHOR = 'Will Mathewson, Nathan Ward'
-PROFILE_IMAGE = 'IMG_0825.png'
+AUTHOR = "Will Mathewson, Nathan Ward"
+PROFILE_IMAGE = "IMG_0825.png"
 BIO = "A post-modern identity crisis with Will & Nathan"
-SITENAME = 'Cabbage & Shame'
-SITEURL = ''
-THEME = 'theme'
+SITENAME = "Cabbage & Shame"
+SITEURL = ""
+THEME = "theme"
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Europe/London'
+TIMEZONE = "Europe/London"
 
-DEFAULT_LANG = 'en'
-GITHUB_URL = 'https://github.com/neanias/cabbageandshame'
+DEFAULT_LANG = "en"
+GITHUB_URL = "https://github.com/neanias/cabbageandshame"
 
-ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
-ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+ARTICLE_URL = "posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/"
+ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -29,33 +29,31 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 DEFAULT_METADATA = {
-    'status': 'draft',
+    "status": "draft",
 }
 
 MARKDOWN = {
-    'extensions': [
-        'pymdownx.emoji',
-        'pymdownx.tilde',
-    ],
-    'extension_configs': {
-        'markdown.extensions.smarty': {},
-        'pymdownx.emoji': {
-            'emoji_index': emoji.twemoji,
-            'emoji_generator': emoji.to_svg,
+    "extensions": ["pymdownx.emoji", "pymdownx.tilde",],
+    "extension_configs": {
+        "markdown.extensions.smarty": {},
+        "pymdownx.emoji": {
+            "emoji_index": emoji.twemoji,
+            "emoji_generator": emoji.to_svg,
         },
     },
 }
 
 # Blogroll
-SIDEBAR_LINKS = (('Categories', '/categories'),
-                 ('Tags', '/tags'),
-                 ('Authors', '/authors'),
-                 )
+SIDEBAR_LINKS = (
+    ("Categories", "/categories"),
+    ("Tags", "/tags"),
+    ("Authors", "/authors"),
+)
 
 # Social widget
-SOCIAL = (('github', 'https://github.com/neanias/cabbageandshame'),)
+SOCIAL = (("github", "https://github.com/neanias/cabbageandshame"),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
--- tasks.py
+++ tasks.py
@@ -10,46 +10,52 @@ from invoke.util import cd
 from pelican.server import ComplexHTTPRequestHandler, RootedHTTPServer
 from pelican.settings import DEFAULT_CONFIG, get_settings_from_file
 
-SETTINGS_FILE_BASE = 'pelicanconf.py'
+SETTINGS_FILE_BASE = "pelicanconf.py"
 SETTINGS = {}
 SETTINGS.update(DEFAULT_CONFIG)
 LOCAL_SETTINGS = get_settings_from_file(SETTINGS_FILE_BASE)
 SETTINGS.update(LOCAL_SETTINGS)
 
 CONFIG = {
-    'settings_base': SETTINGS_FILE_BASE,
-    'settings_publish': 'publishconf.py',
+    "settings_base": SETTINGS_FILE_BASE,
+    "settings_publish": "publishconf.py",
     # Output path. Can be absolute or relative to tasks.py. Default: 'output'
-    'deploy_path': SETTINGS['OUTPUT_PATH'],
+    "deploy_path": SETTINGS["OUTPUT_PATH"],
     # Github Pages configuration
-    'github_pages_branch': 'gh-pages',
-    'commit_message': "'Publish site at {}'".format(datetime.datetime.now().isoformat()),
-    'site_url': 'www.cabbageandshame.co.uk',
+    "github_pages_branch": "gh-pages",
+    "commit_message": "'Publish site at {}'".format(
+        datetime.datetime.now().isoformat()
+    ),
     # Port for `serve`
-    'port': 8000,
+    "port": 8000,
 }
+
 
 @task
 def clean(c):
     """Remove generated files"""
-    if os.path.isdir(CONFIG['deploy_path']):
-        shutil.rmtree(CONFIG['deploy_path'])
-        os.makedirs(CONFIG['deploy_path'])
+    if os.path.isdir(CONFIG["deploy_path"]):
+        shutil.rmtree(CONFIG["deploy_path"])
+        os.makedirs(CONFIG["deploy_path"])
+
 
 @task
 def build(c):
     """Build local version of site"""
-    c.run('pelican -s {settings_base}'.format(**CONFIG))
+    c.run("pelican -s {settings_base}".format(**CONFIG))
+
 
 @task
 def rebuild(c):
     """`build` with the delete switch"""
-    c.run('pelican -d -s {settings_base}'.format(**CONFIG))
+    c.run("pelican -d -s {settings_base}".format(**CONFIG))
+
 
 @task
 def regenerate(c):
     """Automatically regenerate site upon file modification"""
-    c.run('pelican -r -s {settings_base}'.format(**CONFIG))
+    c.run("pelican -r -s {settings_base}".format(**CONFIG))
+
 
 @task
 def serve(c):
@@ -59,12 +65,12 @@ def serve(c):
         allow_reuse_address = True
 
     server = AddressReuseTCPServer(
-        CONFIG['deploy_path'],
-        ('', CONFIG['port']),
-        ComplexHTTPRequestHandler)
+        CONFIG["deploy_path"], ("", CONFIG["port"]), ComplexHTTPRequestHandler
+    )
 
-    sys.stderr.write('Serving on port {port} ...\n'.format(**CONFIG))
+    sys.stderr.write("Serving on port {port} ...\n".format(**CONFIG))
     server.serve_forever()
+
 
 @task
 def reserve(c):
@@ -74,49 +80,54 @@ def reserve(c):
 
 @task
 def preview(c):
-    """Build production version of site"""
-    c.run('pelican -s {settings_publish}'.format(**CONFIG))
+    """Cleans any compiled files then builds production version of site"""
+    c.run("pelican -s {settings_publish}".format(**CONFIG))
+
 
 @task
 def livereload(c):
     """Automatically reload browser tab upon file modification."""
     from livereload import Server
+
     build(c)
     server = Server()
     # Watch the base settings file
-    server.watch(CONFIG['settings_base'], lambda: build(c))
+    server.watch(CONFIG["settings_base"], lambda: build(c))
     # Watch content source files
-    content_file_extensions = ['.md', '.rst']
+    content_file_extensions = [".md", ".rst"]
     for extension in content_file_extensions:
-        content_blob = '{0}/**/*{1}'.format(SETTINGS['PATH'], extension)
+        content_blob = "{0}/**/*{1}".format(SETTINGS["PATH"], extension)
         server.watch(content_blob, lambda: build(c))
     # Watch the theme's templates and static assets
-    theme_path = SETTINGS['THEME']
-    server.watch('{}/templates/*.html'.format(theme_path), lambda: build(c))
-    static_file_extensions = ['.css', '.js']
+    theme_path = SETTINGS["THEME"]
+    server.watch("{}/templates/*.html".format(theme_path), lambda: build(c))
+    static_file_extensions = [".css", ".js"]
     for extension in static_file_extensions:
-        static_file = '{0}/static/**/*{1}'.format(theme_path, extension)
+        static_file = "{0}/static/**/*{1}".format(theme_path, extension)
         server.watch(static_file, lambda: build(c))
     # Serve output path on configured port
-    server.serve(port=CONFIG['port'], root=CONFIG['deploy_path'])
+    server.serve(port=CONFIG["port"], root=CONFIG["deploy_path"])
 
 
 @task
 def publish(c):
     """Publish to production via rsync"""
-    c.run('pelican -s {settings_publish}'.format(**CONFIG))
+    c.run("pelican -s {settings_publish}".format(**CONFIG))
     c.run(
         'rsync --delete --exclude ".DS_Store" -pthrvz -c '
         '-e "ssh -p {ssh_port}" '
-        '{} {ssh_user}@{ssh_host}:{ssh_path}'.format(
-            CONFIG['deploy_path'].rstrip('/') + '/',
-            **CONFIG))
+        "{} {ssh_user}@{ssh_host}:{ssh_path}".format(
+            CONFIG["deploy_path"].rstrip("/") + "/", **CONFIG
+        )
+    )
+
 
 @task
 def gh_pages(c):
     """Publish to GitHub Pages"""
     preview(c)
-    c.run('ghp-import -b {github_pages_branch} '
-          '-m {commit_message} '
-          '-c {site_url} '
-          '{deploy_path} -p'.format(**CONFIG))
+    c.run(
+        "ghp-import -b {github_pages_branch} "
+        "-m {commit_message} "
+        "{deploy_path} -p".format(**CONFIG)
+    )
